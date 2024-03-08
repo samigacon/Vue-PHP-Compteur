12bcfb1d98511e57f1998aa551fe3102906015e0
sami-gacon <sami.gacon@gmail.com>
2024-03-09 00:11:23 +0100

    PHP Only

--- server/api/index.php
+++ server/api/index.php
@@ -10,13 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($data !== null) {
         $compteur = intval($data['compteur']);
         $compteur++;
-
         $response = array("compteur" => $compteur, "echo" => ["Le compteur a été incrémenté avec succès en PHP"]);
         echo json_encode($response);
     } else {
         http_response_code(400);
         echo json_encode(array("message" => "Le contenu de la requête n'est pas au format JSON valide."));
     }
+    
 } else {
     echo "Hello World\n";
 }
